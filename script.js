8f2934691120e592586f5273f7e1ad6e2aea013c
Ankit Sharma <ankitsharma2801@gmail.com>
2023-04-12 12:09:25 +0530

    fix css, add box, add style to container

---
+++ script.js
@@ -0,0 +1,11 @@
+// ondocumentload;
+const container = document.querySelector(".container");
+
+let div;
+//create divs for grid
+for(let i = 0; i < 256; i++) {
+    div = document.createElement("div");
+    div.style.cssText = `height: ${500/16}px; width: ${500/16}px; background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`
+    container.appendChild(div);
+}
+
