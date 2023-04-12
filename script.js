937feadfe92e82683c19c2a2f0b141be1e7e42c4
Ankit Sharma <ankitsharma2801@gmail.com>
2023-04-12 13:26:30 +0530

    add btn node

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// ondocumentload;
 const container = document.querySelector(".container");
+
+const btn = document.querySelector(".chGrid");
+
+
 
 let div;
 //create divs for grid
@@ -8,4 +11,10 @@ for(let i = 0; i < 256; i++) {
     div.style.cssText = `height: ${500/16}px; width: ${500/16}px; background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`
     container.appendChild(div);
 }
+
+btn.addEventListener("onmouseover", () => {
+    
+})
+
+
 
