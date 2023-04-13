ac4b08e5084c14eb00d01a3270f9cde6f351a2c5
Ankit Sharma <ankitsharma2801@gmail.com>
2023-04-13 12:58:21 +0530

    adjust heights and widths for betten viewing

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let div;
 //create divs for grid
 for(let i = 0; i < 256; i++) {
     div = document.createElement("div");
-    div.style.cssText = `height: ${500/16}px; width: ${500/16}px; background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`
+    div.style.cssText = `width: calc(${100}%/${16}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
     container.appendChild(div);
 }
 
