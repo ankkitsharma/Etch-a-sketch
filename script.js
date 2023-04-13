e392b64da673b77595f122b906880379d2eb5b39
Ankit Sharma <ankitsharma2801@gmail.com>
2023-04-13 23:25:09 +0530

    Modularize grid(), add ability to change grid size

--- script.js
+++ script.js
@@ -2,19 +2,47 @@ const container = document.querySelector(".container");
 
 const btn = document.querySelector(".chGrid");
 
+let size = 16; //default grid size 16*16
 
-
-let div;
+let divs = [size*size];
 //create divs for grid
-for(let i = 0; i < 256; i++) {
-    div = document.createElement("div");
-    div.style.cssText = `width: calc(${100}%/${16}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
-    container.appendChild(div);
+function grid() {
+    for(let i = 0; i < (size*size); i++) {
+    divs[i].style.cssText = `width: calc(${100}%/${size}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
+}
 }
 
-btn.addEventListener("onmouseover", () => {
-    
+function createGrid() {
+    for(let i = 0; i < (size*size); i++) {
+    divs[i] = document.createElement("div");
+    container.appendChild(divs[i]);
+}
+}
+
+function deleteGrid() {
+    console.log(size);
+    for(let i = 0; i < (size*size); i++) {
+    container.removeChild(divs[i]);
+    divs[i].remove();
+}
+}
+
+// for(let i = 0; i < (size*size); i++) {
+//     divs[i] = document.createElement("div");
+//     divs[i].style.cssText = `width: calc(${100}%/${size}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
+//     container.appendChild(divs[i]);
+// }
+
+createGrid();
+grid();
+
+btn.addEventListener("click", () => {
+    deleteGrid();
+    size = parseInt(prompt("Enter the grid size: "));
+    createGrid();
+    grid();
 })
+
 
 
 
