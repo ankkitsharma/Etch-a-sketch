b84c7e11a3062a59bcc9d0fa19eae886dfda7bba
Ankit Sharma <ankitsharma2801@gmail.com>
2023-04-13 23:29:40 +0530

    add some cleaning and documentation

--- script.js
+++ script.js
@@ -4,14 +4,9 @@ const btn = document.querySelector(".chGrid");
 
 let size = 16; //default grid size 16*16
 
-let divs = [size*size];
-//create divs for grid
-function grid() {
-    for(let i = 0; i < (size*size); i++) {
-    divs[i].style.cssText = `width: calc(${100}%/${size}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
-}
-}
+let divs = []; //create divs variable
 
+//create square grid of divs
 function createGrid() {
     for(let i = 0; i < (size*size); i++) {
     divs[i] = document.createElement("div");
@@ -19,6 +14,14 @@ function createGrid() {
 }
 }
 
+//add grid style
+function grid() {
+    for(let i = 0; i < (size*size); i++) {
+    divs[i].style.cssText = `width: calc(${100}%/${size}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
+}
+}
+
+//delete grid
 function deleteGrid() {
     console.log(size);
     for(let i = 0; i < (size*size); i++) {
@@ -27,15 +30,12 @@ function deleteGrid() {
 }
 }
 
-// for(let i = 0; i < (size*size); i++) {
-//     divs[i] = document.createElement("div");
-//     divs[i].style.cssText = `width: calc(${100}%/${size}); background-color: #e1e6e5; border-color: black; border-style: solid; border-width: 1px; box-sizing: border-box;`;
-//     container.appendChild(divs[i]);
-// }
+//create and design grid
 
 createGrid();
 grid();
 
+//change grid
 btn.addEventListener("click", () => {
     deleteGrid();
     size = parseInt(prompt("Enter the grid size: "));
